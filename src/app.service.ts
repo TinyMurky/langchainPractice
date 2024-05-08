908dc1510e2386dbaceac24625668d3e76d780b5
TinyMurky <murky0830@gmail.com>
2024-05-08 23:02:28 +0800

    embed

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,7 @@ export class AppService {
 
   start(): string {
     //this.langchainService.invoke();
-    this.langchainService.withPromptTemplate();
+    this.langchainService.splitText();
     return 'Hello World!';
   }
 }
--- src/langchain/langchain.service.ts
+++ src/langchain/langchain.service.ts
@@ -1,23 +1,29 @@
-import { ChatOllama } from '@langchain/community/chat_models/ollama';
+import { Ollama } from '@langchain/community/llms/ollama';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ChatPromptTemplate } from '@langchain/core/prompts';
-
+import { StringOutputParser } from '@langchain/core/output_parsers';
+import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { OllamaEmbeddings } from '@langchain/community/embeddings/ollama';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
+import { writeFileSync } from 'fs';
 @Injectable()
 export class LangchainService {
-  private readonly chatOllama: ChatOllama;
+  private readonly ollama: Ollama;
   private readonly baseUrl: string =
     this.configService.get<string>('OLLAMA_URL');
 
   constructor(private readonly configService: ConfigService) {
-    this.chatOllama = new ChatOllama({
+    this.ollama = new Ollama({
       baseUrl: this.baseUrl,
       model: 'llama3',
     });
   }
 
   public async invoke(): Promise<void> {
-    const invokeResult = await this.chatOllama.invoke('What is LangSmith?');
+    const invokeResult = await this.ollama.invoke('What is LangSmith?');
     console.log(JSON.stringify(invokeResult, null, 2));
   }
 
@@ -27,9 +33,61 @@ export class LangchainService {
       ['user', '{input}'],
     ]);
 
-    const chain = prompt.pipe(this.chatOllama);
+    const chain = prompt.pipe(this.ollama);
 
     const invokeResult = await chain.invoke({ input: 'What is LangSmith?' });
     console.log(JSON.stringify(invokeResult, null, 2));
+  }
+
+  public async withOutputParser(): Promise<void> {
+    const prompt = ChatPromptTemplate.fromMessages([
+      ['system', 'You are the world class LLM engineer.'],
+      ['user', '{input}'],
+    ]);
+
+    const outputParser = new StringOutputParser();
+
+    const chain = prompt.pipe(this.ollama).pipe(outputParser);
+
+    const invokeResult = await chain.invoke({ input: 'What is LangSmith?' });
+    console.log(invokeResult);
+  }
+
+  public async splitText(): Promise<void> {
+    const directoryLoader = new DirectoryLoader(
+      '/home/tinymurky/Documents/IFRS/',
+      {
+        '.pdf': (path: string) => new PDFLoader(path),
+      },
+    );
+
+    const docs = await directoryLoader.load();
+
+    console.log({ docs });
+
+    /* Additional steps : Split text into chunks with any TextSplitter. You can then use it as context or save it to memory afterwards. */
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+
+    const splitDocs = await textSplitter.splitDocuments(docs);
+
+    // 記得要先pull ollama pull nomic-embed-text
+    const embeddings = new OllamaEmbeddings({
+      model: 'nomic-embed-text',
+      baseUrl: this.baseUrl,
+    });
+
+    const vectorStore = await MemoryVectorStore.fromDocuments(
+      splitDocs,
+      embeddings,
+    );
+
+    const json = vectorStore.toJSON();
+    writeFileSync(
+      '/home/tinymurky/Documents/IFRS_vector_store.json',
+      JSON.stringify(json, null, 2),
+    );
   }
 }
